8bb71c543fdbf7139c58a7c0bba2ef407ce9a7fa
a.23308051280387 <a.23308051280387@cbtis128.edu.mx>
2026-02-17 18:13:32 +0000

    act10pasteles

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,110 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const PasteleriaApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PasteleriaApp extends StatelessWidget {
+  const PasteleriaApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CatalogoPasteles(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CatalogoPasteles extends StatelessWidget {
+  const CatalogoPasteles({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<Map<String, String>> pasteles = const [
+    {'nombre': 'red velvet', 'url': 'https://raw.githubusercontent.com/YUridiaCamargo/imagenesp/refs/heads/main/tarta-red-velvet.jpg'},
+    {'nombre': 'chocolate', 'url': 'https://raw.githubusercontent.com/YUridiaCamargo/imagenesp/refs/heads/main/choccoo.jpg'}, // Imagen actualizada
+    {'nombre': 'confeti', 'url': 'https://raw.githubusercontent.com/YUridiaCamargo/imagenesp/refs/heads/main/PASTELCONFETI%20(1).jpg'},
+    {'nombre': 'vainilla', 'url': 'https://raw.githubusercontent.com/YUridiaCamargo/imagenesp/refs/heads/main/vainilla.jpg'},
+    {'nombre': 'nutella', 'url': 'https://raw.githubusercontent.com/YUridiaCamargo/imagenesp/refs/heads/main/Pastelnutella1.png'},
+    {'nombre': 'fresa', 'url': 'https://raw.githubusercontent.com/YUridiaCamargo/imagenesp/refs/heads/main/pastelfresa.png'},
+    {'nombre': 'cajeta', 'url': 'https://raw.githubusercontent.com/YUridiaCamargo/imagenesp/refs/heads/main/autentico-de-cajeta.jpg'},
+    {'nombre': 'ferrero', 'url': 'https://raw.githubusercontent.com/YUridiaCamargo/imagenesp/refs/heads/main/ferrero.jpg'},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color(0xFFEBE6D3),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        toolbarHeight: 70,
+        backgroundColor: const Color(0xFF5D1711),
+        title: const Text(
+          'Pasteles',
+          style: TextStyle(
+            color: Colors.white, 
+            fontFamily: 'Cursive',
+            fontSize: 32,
+          ),
         ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite, color: Colors.white),
+            onPressed: () {}, // Botón de acción
+          ),
+          const SizedBox(width: 10),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: GridView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20), // Más margen lateral para achicar cuadros
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+          childAspectRatio: 0.9, // Ajuste para que el container sea más cuadrado y chico
+        ),
+        itemCount: pasteles.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFFA64D36),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Imagen más grande y proporcional
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    pasteles[index]['url']!,
+                    height: 85, // Altura incrementada
+                    width: 110, // Ancho incrementado
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  pasteles[index]['nombre']!,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+                // Botón de acción "ordenar"
+                InkWell(
+                  onTap: () {},
+                  child: const Text(
+                    'ordenar',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
